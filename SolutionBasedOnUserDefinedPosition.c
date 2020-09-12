eea209d678397d092530d93b310d1e67431a1210
lucaschfonseca <lucaschfonseca@gmail.com>
2020-09-12 10:31:48 -0300

    found a solution

--- SolutionBasedOnUserDefinedPosition.c
+++ SolutionBasedOnUserDefinedPosition.c
@@ -3,116 +3,183 @@
 //
 
 #include <stdio.h>
-#include "SolutionBasedOnUserDefinedPosition.h"
+#include <string.h>
 
-#define QUEEN_COUNT 8
+#include "SolutionBasedOnUserDefinedPosition.h"
+#include "constants.h"
+
+#define BOARD_DIMENSION 8
+
+int initialRow;
 
 int solutionsCount = 0;
-int queens[QUEEN_COUNT];
+int board[BOARD_DIMENSION][BOARD_DIMENSION];
 
-Position entryPoint;
+FILE *f;
 
-Position createPosition(int x, int y) {
-    Position p;
-
-    p.x = x;
-    p.y = y;
-
-    return p;
-}
-
-void printSolution() {
+/**
+ * Saves the solution and its number to a file
+ *
+ * @param solution
+ * @param output
+ */
+void logSolution(FILE *output) {
     int i;
     int j;
 
-    printf("\nSolucao: %d\n", solutionsCount);
+    if (!output)
+        return;
 
-    for (i = 0; i < QUEEN_COUNT; i++) {
-        for (j = 0; j < QUEEN_COUNT; j++) {
-            if (j == queens[i]) {
-                printf("[Q] ");
-            } else {
-                printf("[ ] ");
-            }
+    fprintf(output, "%s %d\n\n ", SOLUTION, solutionsCount);
+
+    for (i = 0; i < BOARD_DIMENSION; i++) {
+        fprintf(output, "  %d ", i);
+    }
+    fprintf(output, "\n");
+
+    for (i = 0; i < BOARD_DIMENSION; i++) {
+        fprintf(output, "%d ", i);
+        for (j = 0; j < BOARD_DIMENSION; j++) {
+            int itemOnPosition = board[i][j];
+            fprintf(output, "[%c] ", itemOnPosition == 1 ? 'Q' : ' ');
         }
 
-        printf("\n");
+        fprintf(output, "\n\n");
     }
 
-    printf("\n");
+    fprintf(output, "\n");
 }
 
-int isAttacked(Position from, Position target) {
-    if ((from.x == target.x) || (from.y == target.y)) // same line or same column
-        return 1;
 
-    if (from.x > target.x)
-        if ((from.x - target.x == from.y - target.y) || (from.x - target.x == target.y - from.y))
-            return 1;
-
-    if (target.x > from.x)
-        if ((target.x - from.x == target.y - from.y) || (target.x - from.x == from.y - target.y))
-            return 1;
-
-    return 0;
-}
-
-int isSafePlace(int k) {
+/**
+ * Checks whether the queen can occupy that row without being attacked
+ *
+ * @param row
+ * @return 1 if true, 0 otherwise
+ */
+int isRowFree(int row) {
     int i;
-    Position target = createPosition(k, queens[k]);
 
-    for (i = 0; i < k; i++) {
-        if (isAttacked(createPosition(i, queens[i]), target))
+    for (i = 0; i < BOARD_DIMENSION; i++) {
+        if (board[row][i] == 1)
             return 0;
     }
 
     return 1;
 }
 
-void solve(int index) {
+/**
+ * Checks whether the queen can occupy that column without being attacked
+ *
+ * @param column
+ * @return 1 if true, 0 otherwise
+ */
+int isColumnFree(int column) {
+    int i;
 
-    if (queens[0] >= QUEEN_COUNT) // already ran through all possible positions and found the solution.
-        return;
-
-    if (queens[index] >= QUEEN_COUNT) // found all solutions based on this position.
-    {
-        index--; // step backward to the previous queen
-        if (index != entryPoint.y) { // we do not move the queen from the entry point
-            queens[index]++; // steps into next queen's position
-        } else
-            index--; // moves bac
-
-        solve(index);
+    for (i = 0; i < BOARD_DIMENSION; i++) {
+        if (board[i][column] == 1)
+            return 0;
     }
 
-    if (index == QUEEN_COUNT - 1 && (isSafePlace(index))) {
-        solutionsCount++;
-        printSolution();
-        queens[index]++;
-
-        solve(index);
-    }
-
-    if (isSafePlace(index)) {
-        index++;
-        queens[index] = 0;
-    } else {
-        queens[index]++;
-    }
-
-    solve(index);
+    return 1;
 }
 
-int findSolution(Position from) {
+/**
+ * Check if the queen can occupy the position without being attacked through the main diagonal
+ *
+ * @param row
+ * @param column
+ * @return 1 if true, 0 otherwise
+ */
+int isDiagonalFree(int row, int column) {
+    int i;
+    int j;
+
+    for (i = row, j = column; i >= 0 && j >= 0; i--, j--) {
+        if (board[i][j] == 1)
+            return 0;
+    }
+
+    for (i = row, j = column; i < BOARD_DIMENSION && j < BOARD_DIMENSION; i++, j++) {
+        if (board[i][j] == 1)
+            return 0;
+    }
+
+    return 1;
+}
+
+/**
+ * Check if the queen can occupy the position without being attacked through the secondary diagonal
+ * @param row
+ * @param column
+ * @return 1 if true, 0 otherwise
+ */
+int isSecondaryDiagonalFree(int row, int column) {
+    int i;
+    int j;
+
+    for (i = row, j = column; i >= 0 && j < BOARD_DIMENSION; i--, j++) {
+        if (board[i][j] == 1)
+            return 0;
+    }
+
+    for (i = row, j = column; i < BOARD_DIMENSION && j >= 0; i++, j--) {
+        if (board[i][j] == 1)
+            return 0;
+    }
+
+    return 1;
+}
+
+int isSafe(int row, int col) {
+    return isRowFree(row)
+           && isColumnFree(col)
+           && isDiagonalFree(row, col)
+           && isSecondaryDiagonalFree(row, col);
+}
+
+void nQueenHelper(int row) {
+    if (row == initialRow) {
+        // We have reached some solution.
+        // Print the board matrix
+        // return
+        solutionsCount++;
+        logSolution(f);
+        return;
+    }
+
+    // Place at all possible positions and move to smaller problem
+    for (int column = 0; column < BOARD_DIMENSION; column++) {
+        if (isSafe(row, column)) {  // if no attack, proceed
+            board[row][column] = 1;
+
+            if (row != initialRow)
+                nQueenHelper((row + 1) % BOARD_DIMENSION);  // call function to continue further
+        }
+
+        board[row][column] = 0; // unmark to backtrack
+    }
+}
+
+void placeQueens(int column, int row) {
     solutionsCount = 0;
 
-    entryPoint = from;
+    initialRow = row;
 
-    queens[0] = 0;
-    for (int i = 1; i < QUEEN_COUNT; i++) {
-        queens[i] = 0;
-    }
-    solve(from.y);
+    memset(board, 0, sizeof(board));
+    board[row][column] = 1;
 
-    return solutionsCount;
+    nQueenHelper(row + 1 % BOARD_DIMENSION); // call the backtracking function and print solutions
+}
+
+int main() {
+    int column = 5;
+    int row = 5;
+
+    f = fopen("output2.txt", "w");
+
+    placeQueens(column, row);
+    printf("%s: %d", NUMBER_OF_POSSIBLE_SOLUTIONS, solutionsCount);
+    return 0;
 }
--- main.c
+++ main.c
@@ -4,37 +4,6 @@
 
 #include "SolutionBasedOnUserDefinedPosition.h"
 #include "constants.h"
-#include "queens_problem.h"
 
-
-int main(int argc, char *argv[]) {
-    setlocale(LC_ALL, "Portuguese");
-
-    char errorMessage[300];
-
-    // user can either or not define the starting column
-    int startingColumn = argc > 2 ? atoi(argv[1]) : 0;
-
-    // user can either or not define the starting line
-    int startingLine = argc == 3 ? atoi(argv[2]) : argc == 2 ? atoi(argv[1]) : 0;
-
-    FILE *f = fopen(OUTPUT_FILE, "w");
-
-    if (!f)
-        printf("%s\n", UNABLE_TO_LOG_SOLUTIONS);
-
-    if (isValidParameters(startingLine, startingColumn, errorMessage) == 0) {
-        printf("\n%s", errorMessage);
-        return 1;
-    }
-
-    printf("%s: %d", NUMBER_OF_POSSIBLE_SOLUTIONS, solveProblem(startingLine, startingColumn, f));
-    if (f)
-        printf("\n%s %s", SOLUTION_GENERATED_IN, OUTPUT_FILE);
-
-    fclose(f);
-
-    return 0;
-}
 
 
